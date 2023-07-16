b1ee572f8202b79661dee7f61976f2e4fd375c4f
ShellCode33 <shellcode33@protonmail.ch>
2023-07-16 22:57:32 +0200

    Add sync script which copies edited files from the git repo to the running OS

---
+++ sync.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -euo pipefail
+cd "$(dirname "$0")"
+
+if [ "$EUID" -ne 0 ]
+then
+    echo "Please run as root using sudo"
+    exit 1
+fi
+
+copy_if_different() {
+    from="$1"
+    to="$2"
+
+    if grep username_placeholder "$from" &>/dev/null
+    then
+        sha1from="$(sed "s/username_placeholder/$SUDO_USER/g" "$from" | sha1sum | awk '{print $1}' || true)"
+    else
+	sha1from="$(sha1sum "$from" 2>/dev/null | awk '{print $1}' || true)"
+    fi
+
+    sha1to="$(sha1sum "$to" 2>/dev/null | awk '{print $1}' || true)"
+
+    if [ "$sha1from" != "$sha1to" ]
+    then
+        mkdir -p "(dirname "$dest")"
+	cp --interactive "$file" "$dest"
+    fi
+}
+
+export -f copy_if_different
+
+find archlinux -type f ! -name "packages-*" -exec bash -c 'file="$1"; dest="/${file#archlinux/}"; copy_if_different "$file" "$dest"' shell {} \;
